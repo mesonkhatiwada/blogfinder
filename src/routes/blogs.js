1db82038244cc317694f89c3008f8a1a45fb2f32
Meson Khatiwada <misonkhatiwada098@gmail.com>
2025-12-24 23:16:01 +0545

    Add route for uploading blogs

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import register from '../controller/register.js';
 import login from '../controller/login.js';
+import uploadBlog from '../controller/uploadBlog.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register',register); // this is the path of the register
+router.post('/blogs',uploadBlog);
 
 export default router;
